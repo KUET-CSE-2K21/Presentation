909004b07b36051128fb622dcc9ed7bb938aa99b
tasmirz <zihad@example.com>
2023-01-27 08:34:07 +0600

    refactor

--- index.js
+++ index.js
@@ -1,39 +1,9 @@
 window.onload = main;
-var section
+var section, data;
 function main() {
 	handleNavigation()
+	data = read_csv(values.data)
 	section = document.querySelector("section")
-	for (i in data) {
-		var namehash = hash(data[i].name);
-		var gradient = values.gradient[Math.abs(namehash % values.gradient.length)];
-		var angle = namehash % 180 * Math.pow(-1, namehash % 10);
-		var gradientText = "background:linear-gradient(" + angle + "deg," + gradient[0] + "," + gradient[1] + ")"
-		var article = document.createElement("article")
-		article.setAttribute("style", gradientText);
-		article.classList.add("hidden")
-		var image = document.createElement("div")
-		image.classList.add("img")
-		image.setAttribute("style", "background:url('" + "images/" + data[i].roll + ".jpg\');background-size:auto 100%;background-repeat:no-repeat;background-position:center center")
-		var imageContainer = document.createElement("figure")
-		var dataContainer = document.createElement("div")
-		dataContainer.classList.add("dcontainer")
-		var name = document.createElement("div")
-		name.classList.add("name")
-		var roll = document.createElement("div")
-		roll.classList.add("roll")
-		var homeDistrict = document.createElement("div")
-		homeDistrict.classList.add("home")
-
-		name.innerHTML = data[i].name
-		roll.innerHTML = data[i].roll
-
-		imageContainer.appendChild(image)
-		dataContainer.appendChild(name)
-		dataContainer.appendChild(roll)
-		dataContainer.appendChild(homeDistrict)
-		article.appendChild(dataContainer)
-		article.appendChild(imageContainer)
-		section.appendChild(article)
-	}
+	renderTemplate(section, data)
 	navigate(0)
 }
---
+++ js/read_csv.js
@@ -0,0 +1,61 @@
+function open_file(url) {
+	const xhrs = new XMLHttpRequest()
+	xhrs.open('GET', url, false)
+	xhrs.send(null)
+
+	if (xhrs.status === 200) {
+		return xhrs.responseText
+	}
+	return false
+}
+function read_csv_header(line) {
+	var ret = []
+	var temp = ""
+	for (i in line) {
+		switch (line[i]) {
+			case ',':
+				ret.push(temp)
+				temp = ""
+				break
+			default:
+				temp += line[i]
+		}
+	}
+	ret.push(temp)
+	return ret
+}
+function read_csv_line(line, header) {
+	var header_at = 0
+	var ret = {}
+	var temp = ""
+	for (i in line) {
+		switch (line[i]) {
+			case ',':
+				ret[header[header_at++]] = temp
+				temp = ""
+				break
+			default:
+				temp += line[i]
+		}
+	}
+	if (header_at < header.length) ret[header[header_at++]] = temp
+	return ret
+}
+function read_csv(url) {
+	var ret = []
+	var csvdata = open_file(url)
+	var csvArray = csvdata.split('\n');
+	var header = read_csv_header(csvArray[0])
+	for (let i = 1; i < csvArray.length; i++) {
+		var linedata = read_csv_line(csvArray[i], header)
+		ret[linedata.roll] = linedata
+	}
+	return ret
+}
+function merge_data(table1, table2) {
+	for (i in table1) {
+		for (j of Object.keys(obj)) {
+			table1[i][j] = table2[i][j]
+		}
+	}
+}
